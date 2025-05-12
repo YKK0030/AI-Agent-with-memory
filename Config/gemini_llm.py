bbf9455a6f07991fb59aa398c9ce50c1e742a358
YKK0030 <kaleyadnit0030@gamil.com>
2025-05-12 10:29:51 +0530

    updates

---
+++ Config/gemini_llm.py
@@ -0,0 +1,14 @@
+from neo4j_graphrag.llm.base import LLMInterface
+from google.generativeai import GenerativeModel  # from `google-generativeai` package
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class GeminiLLM(LLMInterface):
+    def __init__(self, api_key: str = os.getenv("GoogleGeminiKey"), model: str = "gemini-1.5-flash"):
+        self.model = GenerativeModel(model)
+        self.api_key = api_key
+
+    def call(self, prompt: str) -> str:
+        response = self.model.generate_content(prompt)
+        return response.text  # adjust this depending on actual return structure
